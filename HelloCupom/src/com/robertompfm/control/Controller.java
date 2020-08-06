77b7435591336ff0be14a63211a48dcc51a72de3
Roberto Mendes <robertompfm@gmail.com>
2020-08-06 12:26:58 -0300

    first commit

---
+++ HelloCupom/src/com/robertompfm/control/Controller.java
@@ -0,0 +1,4 @@
+package com.robertompfm.control;
+
+public class Controller {
+}
---
+++ HelloCupom/src/module-info.java
@@ -0,0 +1,10 @@
+module HelloCupom {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+
+    opens com.robertompfm;
+
+
+    opens com.robertompfm.control;
+}
