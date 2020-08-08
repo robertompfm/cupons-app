8abec8c2ff5a65703484a12964f132f15a661034
Roberto Mendes <robertompfm@gmail.com>
2020-08-08 15:41:45 -0300

    main window almost complete

--- HelloCupom/src/module-info.java
+++ HelloCupom/src/module-info.java
@@ -2,11 +2,15 @@ module HelloCupom {
 
     requires javafx.fxml;
     requires javafx.controls;
+    requires javafx.graphics;
     requires junit;
     requires java.sql;
+    requires java.base;
+    requires java.xml;
+    requires fontawesomefx;
 
 
-
+    opens com.robertompfm;
     opens com.robertompfm.control;
     opens com.robertompfm.dao;
     opens com.robertompfm.model;
